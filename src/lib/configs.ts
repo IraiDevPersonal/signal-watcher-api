7dfd96f358eabee74e403de3e5257fc38a8ce76d
IraiDevPersonal <ignacio.arr01@gmail.com>
2025-09-12 16:59:40 -0300

    fix: variable de entorno POSTEGRES_BD pasa a ser opcional

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -3,7 +3,7 @@ import { get } from "env-var";
 
 export const ENVS = {
   PORT: get("PORT").required().asPortNumber(),
-  POSTGRES_DB: get("POSTGRES_DB").required().asString(),
+  POSTGRES_DB: get("POSTGRES_DB").default("").asString(),
   POSTGRES_USER: get("POSTGRES_USER").required().asString(),
   POSTGRES_PASSWORD: get("POSTGRES_PASSWORD").required().asString(),
   POSTGRES_URL: get("POSTGRES_URL").required().asString(),
